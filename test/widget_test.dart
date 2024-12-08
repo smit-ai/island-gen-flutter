d5e7740779fb45d56e856de7fd6377f825cfd0de
Mag90 <magnus.ewerlof@gmail.com>
2024-12-08 22:39:04 +0100

    Add: Editor main page with basic layout

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:island_gen_flutter/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    //await tester.pumpWidget(const CounterScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
